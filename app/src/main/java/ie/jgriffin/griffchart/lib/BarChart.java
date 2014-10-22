fe7e0d5ea8e85f75d1b0cbe9677121a705bfe1db
JGriffin <griffin.john.michael@gmail.com>
2014-10-22 20:30:18 +0100

    Addition of BarChart View class.

---
+++ app/src/main/java/ie/jgriffin/griffchart/lib/BarChart.java
@@ -0,0 +1,25 @@
+package ie.jgriffin.griffchart.lib;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by JGriffin on 22/10/2014.
+ */
+public class BarChart extends View{
+    public BarChart(Context context) {
+        super(context);
+    }
+
+    public BarChart(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BarChart(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+}
