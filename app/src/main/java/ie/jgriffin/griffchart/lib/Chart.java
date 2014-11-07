44f9ed32ef50e7e61712e30501e6c35d56a6efa0
JGriffin <griffin.john.michael@gmail.com>
2014-11-07 19:51:20 +0000

    Empty commit to correct comment of last commit. Extraction of common chart functionality from BarChart to a Chart superclass.

--- app/src/main/java/ie/jgriffin/griffchart/lib/Chart.java
+++ app/src/main/java/ie/jgriffin/griffchart/lib/Chart.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
  */
 public class Chart extends View {
 
+
     protected float edgeWidth = 1f;
     protected int viewWidth, viewHeight;
     protected int backgroundColor = Color.WHITE;
