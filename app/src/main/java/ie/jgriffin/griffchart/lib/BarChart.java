6ab7463679ff3affd7a3dcd8a2548b26b48fa374
JGriffin <griffin.john.michael@gmail.com>
2014-11-07 19:51:54 +0000

    Altered BarChart to inherit from Chart.

--- app/src/main/java/ie/jgriffin/griffchart/lib/BarChart.java
+++ app/src/main/java/ie/jgriffin/griffchart/lib/BarChart.java
@@ -2,88 +2,39 @@ package ie.jgriffin.griffchart.lib;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.view.View;
 
-import java.util.ArrayList;
 
 /**
  * Created by JGriffin on 22/10/2014.
  */
-public class BarChart extends View {
+public class BarChart extends Chart {
 
-    private float edgeWidth = 1f;
-    private int viewWidth, viewHeight;
-    private int backgroundColor = Color.WHITE;
 
-    private float chartTopBound, chartBottomBound, chartLeftBound, chartRightBound, chartBoundPadding = 5f;
-    private float chartVerticalSize, chartHorizontalSize;
     private float barPadding = 1f, barWidth, halfBarWidth;
     private int barCount;
     private float dataMax = 100f, scalingFactorBar;
 
-
-    private Paint edgePaint;
-
-    private ArrayList<ChartPoint> chartPoints = new ArrayList<ChartPoint>();
-
     public BarChart(Context context) {
         super(context);
-        initPaints();
     }
 
     public BarChart(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initPaints();
     }
 
     public BarChart(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initPaints();
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        viewWidth = MeasureSpec.getSize(widthMeasureSpec);
-        viewHeight = MeasureSpec.getSize(heightMeasureSpec);
-        setMeasuredDimension(viewWidth, viewHeight);
-    }
-
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        initChartBounds();
-        drawBackground(canvas);
-        drawEdge(canvas);
-
         if(!chartPoints.isEmpty()){
             initBarBounds();
             drawBars(canvas);
         }
-
-    }
-
-    private void initPaints() {
-        edgePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        edgePaint.setColor(Color.BLACK);
-        edgePaint.setStyle(Paint.Style.FILL);
-    }
-
-    private void initChartBounds(){
-        chartTopBound = chartBoundPadding;
-        chartBottomBound = viewHeight - chartBoundPadding;
-        chartLeftBound = chartBoundPadding;
-        chartRightBound = viewWidth - chartBoundPadding;
-        chartVerticalSize = chartBottomBound - chartTopBound;
-        chartHorizontalSize = chartRightBound - chartLeftBound;
     }
 
     private void initBarBounds(){
@@ -115,59 +66,19 @@ public class BarChart extends View {
         return new float[]{left, top, right, bottom};
     }
 
-
-    private void drawBackground(Canvas canvas) {
-        canvas.drawColor(backgroundColor);
+    public float getBarPadding() {
+        return barPadding;
     }
 
-    private void drawEdge(Canvas canvas) {
-        //top
-        canvas.drawLine(edgeWidth, edgeWidth, viewWidth - edgeWidth, edgeWidth, edgePaint);
-        //left
-        canvas.drawLine(edgeWidth, edgeWidth, edgeWidth, viewHeight - edgeWidth, edgePaint);
-        //bottom
-        canvas.drawLine(edgeWidth, viewHeight - edgeWidth, viewWidth - edgeWidth, viewHeight - edgeWidth, edgePaint);
-        //right
-        canvas.drawLine(viewWidth - edgeWidth, edgeWidth, viewWidth - edgeWidth, viewHeight - edgeWidth, edgePaint);
+    public void setBarPadding(float barPadding) {
+        this.barPadding = barPadding;
     }
 
-    public void redraw(){
-        invalidate();
+    public float getDataMax() {
+        return dataMax;
     }
 
-    public int getBackgroundColor() {
-        return backgroundColor;
+    public void setDataMax(float dataMax) {
+        this.dataMax = dataMax;
     }
-
-    public void setBackgroundColor(int backgroundColor) {
-        this.backgroundColor = backgroundColor;
-    }
-
-    public ArrayList<ChartPoint> getChartPoints() {
-        return chartPoints;
-    }
-
-    public void setChartPoints(ArrayList<ChartPoint> chartPoints) {
-        this.chartPoints = chartPoints;
-        invalidate();
-    }
-
-    public float getEdgeWidth() {
-        return edgeWidth;
-    }
-
-    public void setEdgeWidth(float edgeWidth) {
-        this.edgeWidth = edgeWidth;
-    }
-
-    public Paint getEdgePaint() {
-        return edgePaint;
-    }
-
-    public void setEdgePaint(Paint edgePaint) {
-        this.edgePaint = edgePaint;
-    }
-
-
-
 }
